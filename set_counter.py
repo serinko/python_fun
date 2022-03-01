6d8f4e593658c5a22582d673ba9eac79a4877730
serinko <johanesxsalmon@riseup.net>
2022-03-01 09:25:28 +0100

    needed steps

---
+++ set_counter.py
@@ -0,0 +1,10 @@
+"""A simple counter tracking your excercising sets"""
+
+# TODO:
+# Prompt user to enter name of the excercises
+# Promt user how many sets they want to do
+# Possibly series of sets - can be as an update
+#
+# Make a counter based on enter hits
+# Print each round count
+# Print stop message
