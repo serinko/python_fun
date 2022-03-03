65b5a8cb81b8f79334ffe321878cf4f15b1bbc55
serinko <johanesxsalmon@riseup.net>
2022-03-03 10:22:56 +0100

    Is your birthday contained in Pi

---
+++ birthday_in_Pi.py
@@ -0,0 +1,23 @@
+"""SE if your birthday is in Pi number"""
+
+filename = 'pi_milion_decimals.txt'
+with open(
+        filename
+) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+birthday = input("Hello. \nDo you think your birthday is in the first 1000000 "
+                 "decimals of Pi number?\n\n"
+                 "Enter your birthday in the form ddmmyy: ")
+if birthday in pi_string:
+    print(
+        "\nYour birthday appears in the first milion digits of pi!"
+    )
+
+else:
+    print("\nIt seems it does not. Have a great day")
+   
