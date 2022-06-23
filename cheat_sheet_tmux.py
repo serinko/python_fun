4f483670e3c8f547fcb6f38bb8de05111b437f64
serinko <johanesxsalmon@riseup.net>
2022-06-23 10:51:37 +0200

    tmux windows

--- cheat_sheet_tmux.py
+++ cheat_sheet_tmux.py
@@ -1,10 +1,10 @@
 from tabulate import tabulate
 
 intro = ("\n\nCOMMAND EXPLANATION:\n"
-         "----------","\n$ = Bash terminal command\n: = Tmux command\n"
+         ,"\n$ = Bash terminal command\n: = Tmux command\n"
     "[KEY] = shortcut Tmux")
 
-session = ("TMUX SESSIONS:\n----------\n",)
+session = ("\nTMUX SESSIONS:\n----------\n",)
 run = ("Run tmux:\n","$ tmux")
 run_new = ("Start new session:\n", "$ tmux new\n$ tmux new-session\n: new")
 start_session = ("Starts new session called 'one':","\n$ tmux new -s one\n"
@@ -14,5 +14,11 @@ ls_sessions = ("Shows all sessions:","\n$ tmux ls\n$ tmux list-sessions"\
 
 sessions = [intro, session, run, run_new, start_session, ls_sessions]
 
-print(tabulate(sessions))
+window = ("\nTMUX WINDOWS:\n----------\n",)
+new_w = ("Starts new window with the name 'one' and window 'onewindow':",
+         "\n$ tmux new -s one -n onewindow")
+
+windows = [window,new_w]
+
+print(tabulate(sessions),tabulate(windows))
 
