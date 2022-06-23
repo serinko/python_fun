e7b0e778bcba5fc36c08a47c0aa273e5324bc504
serinko <johanesxsalmon@riseup.net>
2022-06-23 10:35:48 +0200

    tmux sessions

---
+++ cheat_sheet_tmux.py
@@ -0,0 +1,17 @@
+from tabulate import tabulate
+
+intro = ("Comman explanation:\n","$ = Bash terminal command\n: = Tmux command\n"\
+    "[KEY] = shortcut Tmux")
+
+session = ("TMUX SESSIONS:\n ==========\n","")
+run = ("Run tmux:\n","$ tmux")
+run_new = ("Start new session:\n", "$ tmux new\n$ tmux new-session\n: new")
+start_session = ("Starts new session called 'one':","\n$ tmux new -s one\n"
+                                                    ": new -s one")
+ls_sessions = ("Shows all sessions:","\n$ tmux ls\n$ tmux list-sessions"\
+    "\n[Ctrl]+[b] .. [s]")
+
+sessions = [session, run, run_new, start_session, ls_sessions]
+
+print(tabulate(sessions))
+
